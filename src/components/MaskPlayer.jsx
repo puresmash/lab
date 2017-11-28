22f266a7e9acf9e506a837c3289050010baea7d8
--local <puresmash@gmail.com>
2017-11-29 03:34:27 +0800

    Modify MaskPlayer structure

--- src/components/MaskPlayer.jsx
+++ src/components/MaskPlayer.jsx
@@ -5,54 +5,38 @@ import { getViewport } from '../utils/CommonUtils';
 import './MaskPlayer.css';
 
 const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-const Content = ({ children, start }) => {
-  // const start = { height: 0, width: 0 };
-  console.warn(start)
+
+const Wrapper = ({ children, style }) => <div className="modal-wrapper" style={style}>{children}</div>;
+const BackLayer = (props) => <div {...props} className="bg" />;
+const Control = ({ children }) => <div className="control">{children}</div>
+const ControlItem = ({ className, isLast, label }) =>
+  <i className={className} style={isLast ? { paddingRight: 16 } : {}}>
+    <span style={{ paddingLeft: 8 }}>{label}</span>
+  </i>;
+// const ControlArrow =
+const Content = ({ children }) => <div className="content">{children}</div>;
+
+const AnimeBox = ({ children, start }) => {
   const viewport = getViewport();
   const defaultStyle = {
-    height: start.h,
-    width: start.w,
-    left: start.x,
-    top: start.y
+    height: start.h * 100 / viewport.y, width: start.w * 100 / viewport.x,
+    left: start.x, top: start.y
   }
-  const end = {
-    height: spring(viewport.y * 0.8),
-    width: spring(viewport.x),
-    left: spring(0),
-    top: spring(viewport.y * 0.1)
+  const style = {
+    height: spring(80), width: spring(100),
+    left: spring(0), top: spring(viewport.y * 0.1)
   };
-  console.log(defaultStyle);
-  console.log(end)
+  console.warn(children)
   return (
-    <Motion defaultStyle={defaultStyle} style={end}>
+    <Motion defaultStyle={defaultStyle} style={style}>
       {(style) => (
-        <div className="content" style={{
-          width: `${style.width}px`,
-          height: `${style.height}px`,
+        <children.type {...children.props} style={{
+          ...children.props.style,
+          width: `${style.width}vw`,
+          height: `${style.height}vh`,
           left: style.left,
           top: style.top
-        }}>
-          {children}
-        </div>
-      )}
-    </Motion>
-  );
-};
-const Header = ({ children, style }) => <div style={style} className="modal-header">{children}</div>;
-const Body = ({ children }) => <div className="modal-body">{children}</div>
-
-const AnimeBox = ({ children }) => {
-  const start = { height: 0, width: 0 };
-  const end = { height: spring(100), width: spring(100) };
-  return (
-    <Motion defaultStyle={start} style={end}>
-      {(style) => (
-        <div style={{
-          width: `${style.width}%`,
-          height: `${style.height}%`
-        }}>
-          {children}
-        </div>
+        }} />
       )}
     </Motion>
   );
@@ -75,36 +59,30 @@ class MaskPlayer extends Component {
           <button className="btn btn-close" onClick={onClose}>
             <i className="fa fa-times" />
           </button>
+
           { open && (
-
-            <Content start={start}>
-              <div className="c-panel">
-                <div className="control" onClick={onPrevious}>
-                  <i className="fa fa-chevron-left" />
-                </div>
-                <div className="control" onClick={onNext}
-                  style={{ flex: 9, justifyContent: 'flex-end' }}>
-                  <i className="fa fa-chevron-right" />
-                </div>
-              </div>
-              <div style={{
-                  backgroundColor: 'lightseagreen',
-                  width: '100%',
-                  height: '100%'
-              }}>
-                {children}
-              </div>
-              <Header style={closeTitle ? { height: 32 } : { height: 118 }}>
-                <div className="title">
-                  {'TITLE'}
-                  <button className="btn" onClick={this.handleToogleTitle}>
-                    <i className={`fa fa-caret-${closeTitle ? 'up' : 'down'}`} />
-                  </button>
-                </div>
-                <span className="desc" style={closeTitle ? { opacity: 0 } : {}}>{lorem}</span>
-              </Header>
-            </Content>
-
+            <AnimeBox start={start}>
+              <Wrapper>
+                <BackLayer onClick={onClose}/>
+                <Control>
+                  <div className="top">{'TITLE'}</div>
+                  <div className="arrow-layer">
+                    <div className="arrow" onClick={onPrevious}>
+                      <i className="fa fa-chevron-left" />
+                    </div>
+                    <div className="arrow" onClick={onNext}
+                      style={{ flex: 3, justifyContent: 'flex-end' }}>
+                      <i className="fa fa-chevron-right" />
+                    </div>
+                  </div>
+                  <div className="bottom">
+                    <ControlItem isLast className="fa fa-thumbs-o-up" label="讚" />
+                    <ControlItem className="fa fa-star-o" label="封面" />
+                  </div>
+                </Control>
+                <Content>{children}</Content>
+              </Wrapper>
+            </AnimeBox>
           )}
       </div>
     );
